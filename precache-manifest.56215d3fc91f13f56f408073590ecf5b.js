a1ef07caec8694384e932fe5e51c287418c45dfa
ZachZeng <zachzeng@umich.edu>
2020-08-07 17:45:14 -0700

    Updates

---
+++ precache-manifest.56215d3fc91f13f56f408073590ecf5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf9be8441b05aa4d0fc6b943212fc91",
+    "url": "/wz-excel-process/index.html"
+  },
+  {
+    "revision": "a7f27d5b6c2d62350d2d",
+    "url": "/wz-excel-process/static/css/2.9e0dcc19.chunk.css"
+  },
+  {
+    "revision": "fc45c175263aceb247fb",
+    "url": "/wz-excel-process/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a7f27d5b6c2d62350d2d",
+    "url": "/wz-excel-process/static/js/2.df6f321c.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/wz-excel-process/static/js/2.df6f321c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc45c175263aceb247fb",
+    "url": "/wz-excel-process/static/js/main.df7a2530.chunk.js"
+  },
+  {
+    "revision": "e3ccea7010323c3b8cd4",
+    "url": "/wz-excel-process/static/js/runtime-main.1326b3db.js"
+  },
+  {
+    "revision": "c59164def3bab75d3ebda040b18ad3b2",
+    "url": "/wz-excel-process/static/media/excel.c59164de.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wz-excel-process/precache-manifest.56215d3fc91f13f56f408073590ecf5b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wz-excel-process/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
